8042df67a90d5d93c0fa325ffdd486bd87b65f76
Joonho Kim <lippoint.surf0622@gmail.com>
2025-07-22 10:09:01 +0900

    footer 문구 변경

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ import Links from '../links/Links';
 function Footer() {
   return (
     <footer className='text-center text-sm py-8 space-y-3 mt-5 border-t border-border'>
-      <div>Thank you for visiting my portfolio 💻</div>
+      <div>Open to new opportunities.</div>
       <Links />
       <div className='mt-2'>© 2025 Joonho Kim. All rights reserved.</div>
     </footer>
