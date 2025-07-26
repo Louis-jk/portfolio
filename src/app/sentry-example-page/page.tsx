10e42dcfc380ae02fcdffec7079508c052632fc6
Joonho Kim <lippoint.surf0622@gmail.com>
2025-07-26 10:58:54 +0900

    sentry update

--- src/app/sentry-example-page/page.tsx
+++ src/app/sentry-example-page/page.tsx
@@ -40,7 +40,7 @@ export default function Page() {
         </h1>
 
         <p className="description">
-          Click the button below, and view the sample error on the Sentry <a target="_blank" href="https://freelancer-nz-mv.sentry.io/issues/?project=4509732102144000">Issues Page</a>.
+          Click the button below, and view the sample error on the Sentry <a target="_blank" href="https://freelancer-nz-mv.sentry.io/issues/?project=4509732311334912">Issues Page</a>.
           For more details about setting up Sentry, <a target="_blank"
            href="https://docs.sentry.io/platforms/javascript/guides/nextjs/">read our docs</a>.
         </p>
